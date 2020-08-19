b850ba3e365bb2cb9f680e400c6ca0ce4ecbcd1d
huo <644094283@qq.com>
2020-08-19 11:49:14 +0800

    我是信建立的lala

---
+++ src/components/lala.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+ {{info}}
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return{
+        info:"程程吃片片 "
+      }
+    }
+    
+  }
+</script>
+
+<style  scoped>
+
+</style>
